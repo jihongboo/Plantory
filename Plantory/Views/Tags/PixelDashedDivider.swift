afd16d7edad41ce32b26b0f3c1ceb49cb98e56c1
Bobo <jihongboo@icloud.com>
2026-06-11 13:14:15 +0800

    fix build issue

--- Plantory/Views/Tags/PixelDashedDivider.swift
+++ Plantory/Views/Tags/PixelDashedDivider.swift
@@ -40,7 +40,7 @@ struct PixelDashedDivider: View {
     }
 }
 
-private struct PixelSheetDashedLine: Shape {
+private nonisolated struct PixelSheetDashedLine: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
         path.move(to: CGPoint(x: rect.minX, y: rect.midY))
